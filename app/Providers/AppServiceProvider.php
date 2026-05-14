742c5e42c5599d118964798770eb4c6524bc294c
mohammadquds <mohammedq2099@gmail.com>
2026-05-14 12:48:13 +0300

    new change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 
     /**
